2038256069f2264321fdfd567d7daec8521f063e
Kevin Murray <k.d.murray.91@gmail.com>
2013-02-14 18:26:28 +1100

    moved config filename check to get_config

--- conviron/__init__.py
+++ conviron/__init__.py
@@ -3,17 +3,28 @@ try:
 except ImportError:
     # Python 3.x
     from configparser import ConfigParser
+import sys
+import os
 
 
 CONFIG_FILE = "./conviron.ini"  # Default file name
-def get_config(filename):
+def get_config():
     """Returns a ConfigParser which has read the given filename. If filename is
     not given, uses CONFIG_FILE."""
-    if not filename:
-        filename = CONFIG_FILE
+    # If the config file is specified on the command line, use it
+    if len(sys.argv) > 1:
+        if os.path.isfile(sys.argv[1]):
+            config_file = sys.argv[1]
+        else:
+            print("Incorrect commandline.\n"
+                "Format is `python -m conviron <ini_file_path>`")
+            sys.exit(1)
+    else:
+        config_file = CONFIG_FILE
+
     parser = ConfigParser()
     try:
-        parser.readfp(open(filename))
+        parser.readfp(open(config_file))
     except AttributeError:
-        parser.read_file(open(filename))
+        parser.read_file(open(config_file))
     return parser
