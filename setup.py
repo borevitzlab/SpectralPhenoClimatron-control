22bb3f05bbd8af291319442c67aa1e06c27f5ab0
Kevin Murray <spam@kdmurray.id.au>
2014-07-10 15:03:50 +1000

    [setup.py] Bump to v0.2.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name="spcControl",
     install_requires=['psycopg2>=2.4.6', ],
     packages=['spcControl', 'spcControl.monitor',],
-    version="0.2.2",
+    version="0.2.3",
     description=describe,
     author="Kevin Murray",
     author_email="k.d.murray.91@gmail.com",
